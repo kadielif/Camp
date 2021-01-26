ed975bbd2c4fc66551758749915f5fa23946f819
kadielif <elif_kadi@hotmail.com>
2021-01-26 14:06:25 +0300

    26.01.2021 4. Ders

---
+++ OOP1/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product //Entity
+    {
+        public int Id { get; set; }//PrimaryKey
+        public int CategoryId { get; set; } //ForeignKey
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+    }
+}
